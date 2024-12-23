d9a98e39642291a0c95064519d6f5414be9823a6
Pankaj Singh Bisht <74168993+pbisht2105@users.noreply.github.com>
2024-12-23 14:52:12 +0530

    Update Netflix Schemas.sql

--- Netflix Schemas.sql
+++ Netflix Schemas.sql
@@ -1,20 +1,20 @@
 -- SCHEMAS of Netflix
 
-DROP TABLE IF EXISTS netflix;
-CREATE TABLE netflix
-(
-	show_id	VARCHAR(5),
-	type    VARCHAR(10),
-	title	VARCHAR(250),
-	director VARCHAR(550),
-	casts	VARCHAR(1050),
-	country	VARCHAR(550),
-	date_added	VARCHAR(55),
-	release_year	INT,
-	rating	VARCHAR(15),
-	duration	VARCHAR(15),
-	listed_in	VARCHAR(250),
-	description VARCHAR(550)
+DROP table if exists netflix;
+
+CREATE TABLE netflix (
+    show_id VARCHAR(7),
+    show_type VARCHAR(10),
+    title VARCHAR(150),
+    director VARCHAR(210),
+    casts VARCHAR(1000),
+    country VARCHAR(150),
+    date_added VARCHAR(50),
+    release_year INT,
+    rating VARCHAR(10),
+    duration VARCHAR(15),
+    listed_in VARCHAR(150),
+    descriptions VARCHAR(250)
 );
 
 SELECT * FROM netflix;
